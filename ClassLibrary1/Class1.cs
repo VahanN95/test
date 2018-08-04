27dfa2ec66fd46661afc21c1766d86ff1e36399f
Vahan Hakobyan <v.hakobyan@mic.local>
2018-08-04 14:10:57 +0400

    adding new files

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public  string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+
+        }
+
+        public void F(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ DynamicKeyword/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Threading;
+
+namespace DynamicKeyword
+{
+    class Program
+    {
+
+        class Finalizable
+        {
+            ~Finalizable()
+            {
+                Console.WriteLine("Finalizer");
+            }
+        }
+        class WeakReferenceTracker
+        {
+            private readonly WeakReference _wr;
+
+            public WeakReferenceTracker(object o, bool trackResurection)
+            {
+                _wr = new WeakReference(o, trackResurection);
+                // hetevum enq erba mernum
+                Task.Factory.StartNew(TrackDeath);
+            }
+
+            public Action ReferenceDied = () => { };
+
+            // stugum enq ardyoq kendani e
+            private void TrackDeath()
+            {
+                while (true)
+                {
+                    if (!_wr.IsAlive)
+                    {
+                        ReferenceDied();
+                        break;
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating 2 trackers...");
+            Thread.Sleep(1000);
+            var finalizable = new Finalizable();
+
+
+            var weakTracker = new WeakReferenceTracker(finalizable, false);
+            weakTracker.ReferenceDied += () => Console.WriteLine("Short weak reference is dead");
+            //
+            var resurectionTracker = new WeakReferenceTracker(finalizable, true);
+            resurectionTracker.ReferenceDied += () => Console.WriteLine("Long weak reference is dead");
+
+            Console.WriteLine("Forcing 0th generation GC...");
+            GC.Collect(0);
+            Thread.Sleep(100);
+
+            Console.WriteLine("Forcing 1th generation GC...");
+            GC.Collect(1);
+            Thread.Sleep(100);
+
+            // vor chjnji Tracker ner@
+            GC.KeepAlive(weakTracker);
+            GC.KeepAlive(resurectionTracker);
+
+            //            Creating 2 trackers...
+            //            Forcing 0th generation GC...
+            //            Finalizable.dtor
+            //            Short weak reference is dead
+            //            Forcing 1th generation GC...
+            //            Long weak reference is dead
+        }
+    }
+}
